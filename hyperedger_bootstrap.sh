9359e557823ed12ddbeb495ab39b58f724568f72
gabrielpsilva <gabrielpe@gmail.com>
2017-08-07 01:54:59 -0400

    update versions

--- hyperedger_bootstrap.sh
+++ hyperedger_bootstrap.sh
@@ -60,6 +60,7 @@ apt-get update -qq
 apt-get install -y -qq apparmor docker-ce
 
 #"Inslling docker-compose" 
+pip install docker-compose
 #curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 #chmod +x /usr/local/bin/docker-compose
 
