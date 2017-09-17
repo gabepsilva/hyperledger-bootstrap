864daf15019ba0df3eddc6697a2195d7cc962dbe
gabrielpsilva <gabrielpe@gmail.com>
2017-09-17 17:02:19 -0400

    Create ubuntu_docker.sh

---
+++ ubuntu_docker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -x
+
+#force root
+if [ xroot != x$(whoami) ]
+then
+ echo "You must run as root"
+ exit
+fi
+
+apt-get update
+
+apt-get install -y \
+  linux-image-extra-$(uname -r) \
+  linux-image-extra-virtual
+
+apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
+  
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+apt-get update
+
+
+sudo apt-get install docker-ce
+
+docker run hello-world
+
